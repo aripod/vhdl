8d7528a9888e4eff08e6299e79bfce9d17216e85
Ariel <aripod@gmail.com>
2017-09-18 10:39:22 +0200

    Added for...generate statement to have the option to choose the width of the debouncer (multiple buttons on the same IP)

--- Debouncer/Debouncer.vhd
+++ Debouncer/Debouncer.vhd
@@ -2,23 +2,27 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use work.functions.all;
-
+  
 entity Debouncer is
-	Generic ( T : integer := 10);	-- Time (in ms) for the delay.
+	Generic ( T : integer := 10;   -- Time (in ms) for the delay.
+	          N : integer := 5 );  -- Number of buttons to debounce.
 	Port ( clk : in  STD_LOGIC;
-          input : in  STD_LOGIC;
-          output : out  STD_LOGIC);
+          in_button : in  STD_LOGIC_VECTOR(N-1 downto 0);
+          out_debounced : out  STD_LOGIC_VECTOR(N-1 downto 0));
 end Debouncer;
 
 architecture Behavioral of Debouncer is
 
+    type delay_matrix is array (0 to N-1, 0 to 1) of STD_LOGIC;
+
 	signal ms_counter : STD_LOGIC_VECTOR(15 downto 0) := (others=>'0');
 	signal ms_pulse : STD_LOGIC := '0';
-	signal delay : STD_LOGIC_VECTOR (1 downto 0);
+	signal delay : delay_matrix;
 	signal rise_edge : STD_LOGIC;
 	signal count_enable : STD_LOGIC := '0';
-	signal timer_count : STD_LOGIC_VECTOR (log2(T) downto 0) := (others=>'0');
-	signal reset, debounce : STD_LOGIC := '0';
+    signal timer_count : STD_LOGIC_VECTOR (log2(T) downto 0) := (others=>'0');
+	signal debounce : STD_LOGIC_VECTOR(N-1 downto 0) := (others=>'0');
+	signal reset : STD_LOGIC;
 begin
 
 	miliseconds: process(clk)	-- Generates a pulse (1 clk period length) every 1 ms.
@@ -34,39 +38,30 @@ begin
 		end if;
 	end process;
 
-	sync_and_edge_detection : process(clk)	-- Detects rising and falling edge.
-		begin
-		if(rising_edge(clk)) then
-			delay(0) <= delay(1);
-			delay(1) <= input;
-		end if;
-	end process;
-	rise_edge <= delay(1) and (not delay(0));
-	
-	trigger_count: process(clk)
-		begin
-		if(reset='1') then
-			count_enable <= '0';
-		elsif(rising_edge(clk)) then
-			if(rise_edge='1') then
-				count_enable <='1';
-			end if;
-		end if;
-	end process;
-
-	count_proc: process(clk)
-		begin
-		if(rising_edge(clk)) then
-			if(timer_count=T) then
-				timer_count <= (others=>'0');
-				debounce <= input;
-				reset <= '1';
-			elsif(ms_pulse='1') then
-				timer_count <= timer_count + '1';
-				reset <= '0';
-			end if;
-		end if;
-	end process;
-	output <= debounce;
+    generate_statement: for I in 0 to N-1 generate
+        sync_and_edge_detection : process(clk)	-- Detects rising and falling edge.
+            begin
+            if(rising_edge(clk)) then
+                delay(I,0) <= delay(I,1);
+                delay(I,1) <= in_button(I);
+            end if;
+        end process;
+        rise_edge <= delay(I,1) and (not delay(I,0));
+    
+        count_proc: process(clk)
+            begin
+            if(rising_edge(clk)) then
+                if(timer_count=T) then
+                    timer_count <= (others=>'0');
+                    debounce(I) <= in_button(I);
+                    reset <= '1';
+                elsif(ms_pulse='1') then
+                    timer_count <= timer_count + '1';
+                    reset <= '0';
+                end if;
+            end if;
+        end process;
+        out_debounced(I) <= debounce(I);
+    end generate generate_statement;
 	
 end Behavioral;
